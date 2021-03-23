25f205b13b46fcbfdc65dd83245284a79cb0ced0
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2021-03-23 14:44:19 +0900

    Add Python file [1408.py]

---
+++ 1408.py
@@ -0,0 +1,11 @@
+now = list(input())
+start = list(input())
+
+slist = [int(start[0] + start[1]), int(start[3] + start[4]), int(start[6] + start[7])]
+nlist = [int(now[0] + now[1]), int(now[3] + now[4]), int(now[6] + now[7])]
+
+result = []
+if(nlist - slist >= 0):
+    pass
+else:
+    pass
