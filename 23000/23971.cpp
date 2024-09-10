41ca81e1346b5f258ca32d6c91cd37b180d41fe0
dl-00-e8 <sso07012@gmail.com>
2024-09-10 23:59:10 +0900

    Math / B3

---
+++ 23000/23971.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+
+int H, W, N, M;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> H >> W >> N >> M;
+
+    return 0;
+}
