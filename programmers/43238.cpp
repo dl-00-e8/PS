4e3f3fec1b366ed4310e0483898badb1c64239d6
dl-00-e8 <sso07012@gmail.com>
2025-01-19 23:32:35 +0900

    Binaray Search / Lv3

--- programmers/43238.cpp
+++ programmers/43238.cpp
@@ -5,6 +5,12 @@ using namespace std;
 long long solution(int n, vector<int> times) {
     long long answer = 0;
     
+    int start = 0;
+    int end = n - 1;
+
+    while(start <= end) {
+        int mid = (start + end) / 2;
+    }
     
     return answer;
 }
