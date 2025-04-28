a1d02906df78b82cd4bbbed0a2f7735d5aebab53
dl-00-e8 <sso07012@gmail.com>
2025-04-28 21:19:57 +0900

    Binary Search / Lv3

--- programmers/43238.cpp
+++ programmers/43238.cpp
@@ -5,11 +5,25 @@ using namespace std;
 long long solution(int n, vector<int> times) {
     long long answer = 0;
     
-    int start = 0;
-    int end = n - 1;
-
+    sort(times.begin(), times.end());
+    
+    long long start = 0;
+    long long end = (long long)times[times.size() - 1] * n;
+    
     while(start <= end) {
-        int mid = (start + end) / 2;
+        long long mid = (start + end) / 2;
+        
+        long long cnt = 0;
+        for(int i = 0; i < times.size(); i++) {
+            cnt += mid / times[i];
+        }
+        
+        if(cnt < n) {
+            start = mid + 1;
+        } else {
+            answer = mid;
+            end = mid - 1;
+        }
     }
     
     return answer;
